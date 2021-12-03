f6796d634b2f9f770193f21714be72fbf1f33973
Zhao Zhiyu <zhaozhiyu51@gmail.com>
2021-12-03 19:32:33 +0800

    before calc more xi wi

--- 作业6/jsff6_2.f90
+++ 作业6/jsff6_2.f90
@@ -1,16 +1,24 @@
 subroutine problem2()
     implicit none
     integer, parameter :: dp = selected_real_kind(15)
-    real(8), external :: g, composite_simpson, composite_tixing
+    real(8), external :: g, u, composite_simpson, composite_tixing, gauss_raguel
     real(8), dimension(5) :: gamma_xs = [1.0_dp, 5.0_dp, 10.0_dp, 2.333333_dp, 3.141593_dp]
     real(8) :: a = 0.0_dp, b = 60.0_dp
     integer :: n, i
     
     print *, "Problem 2" 
-    ! call search(a, b, n, composite_simpson, g, 1e-6, gamma_x)
-    ! print *, "n=", n, "a=", a, "b=", b, "gamma_x=", gamma_x, "gamma=", composite_simpson(a, b, n, g, gamma_x)
+
+    print *, "Composite Simpson:"
     do i = 1, 5
-        ! b = gamma_xs(i) * 6.0_dp
         print *, composite_simpson(a, b, n, g, 1e-6_dp, gamma_xs(i)), n, gamma_xs(i)
     end do
+
+    n = 5
+    print *, "Gauss-Raguel:"
+    do i = 1, 5
+        print *, gauss_raguel(n, u, gamma_xs(i)), n, gamma_xs(i)
+    end do
+
+
+
 end subroutine problem2
