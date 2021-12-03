1476e8b3572df13c7df0e7b56543f6105ed8f2fa
Zhao Zhiyu <zhaozhiyu51@gmail.com>
2021-12-03 20:04:13 +0800

    before merge data21-40

--- 作业6/jsff6_1.f90
+++ 作业6/jsff6_1.f90
@@ -1,14 +1,12 @@
-subroutine problem1()   
+subroutine problem1()
+       
     implicit none
     integer, parameter :: dp = selected_real_kind(15)
     real(8), external :: f, composite_simpson, composite_tixing
     integer :: n_simpson, n_tixing
 
     print *, "Problem 1"
-    ! call search(0.0_dp, 1.0_dp, n_simpson, composite_simpson, f, 1e-6_dp, 0.0_dp)
-    ! call search(0.0_dp, 1.0_dp, n_tixing, composite_tixing, f, 1e-6_dp, 0.0_dp)
-    ! print *, n_simpson, composite_simpson(0.0_dp, 1.0_dp, n_simpson, f, 0.0_dp)
-    print *, composite_simpson(0.0_dp, 1.0_dp, n_simpson, f, 1e-6_dp, 0.0_dp), n_simpson 
-    print *, composite_tixing(0.0_dp, 1.0_dp, n_tixing, f, 1e-6_dp, 0.0_dp), n_tixing 
+    print *, composite_simpson(0.0_dp, 1.0_dp, n_simpson, f, 1e-6_dp, 0.0_dp), n_simpson
+    print *, composite_tixing(0.0_dp, 1.0_dp, n_tixing, f, 1e-6_dp, 0.0_dp), n_tixing, "\n"
 
 end subroutine problem1
