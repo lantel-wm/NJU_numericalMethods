f0a07dc55c95a2b144ef9f55cd6dbdc98df0ed6d
Zhao Zhiyu <zhaozhiyu51@gmail.com>
2021-12-10 20:13:26 +0800

    make run

--- 作业7/xls2arr.py
+++ 作业7/xls2arr.py
@@ -11,20 +11,20 @@ dfV = pd.read_excel('v.xls', usecols=range(1, 18))
 
 U = dfU.values.transpose()
 V = dfV.values.transpose()
-U = np.insert(U, 0, values=zeros(17), axis=0)
-U = np.insert(U, 18, values=zeros(17), axis=0)
-U = np.insert(U, 0, values=zeros(19), axis=1)
-U = np.insert(U, 18, values=zeros(19), axis=1)
+U = np.insert(U, 0, values=np.zeros(17), axis=0)
+U = np.insert(U, 18, values=np.zeros(17), axis=0)
+U = np.insert(U, 0, values=np.zeros(19), axis=1)
+U = np.insert(U, 18, values=np.zeros(19), axis=1)
 
 np.set_printoptions(linewidth=np.inf)
 with open('u.txt', 'w') as f:
     f.write(' ')
-    f.write(re.sub('[\[\]]', '', np.array_str(U.values.transpose())))
+    f.write(re.sub('[\[\]]', '', np.array_str(U)))
     f.close()
 
 with open('v.txt', 'w') as f:
     f.write(' ')
-    f.write(re.sub('[\[\]]', '', np.array_str(V.values.transpose())))
+    f.write(re.sub('[\[\]]', '', np.array_str(V)))
     f.close()
 
 with open('gird.txt', 'w') as f:
