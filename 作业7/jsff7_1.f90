30be519d3c131f44ac0ab89e05e64c39540d7551
Zhao Zhiyu <zhaozhiyu51@gmail.com>
2021-12-10 20:20:14 +0800

    make run

--- 作业7/jsff7_1.f90
+++ 作业7/jsff7_1.f90
@@ -1,10 +1,10 @@
 subroutine step1()
     implicit none
-    real(8) :: U(17, 17), V(17, 17)
+    real(8) :: U(0:18, 0:18), V(0:18, 0:18)
     real(8) :: grid_X(17, 17), grid_Y(17, 17)
 
     call read_uv(U, V)
-    print *, U
+    print *, U(0, :)
     call read_grid(grid_X, grid_Y)
 
 end subroutine
