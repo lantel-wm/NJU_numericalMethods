7a905712ade5b7d587b9aabea3124969b4f89327
Zhao Zhiyu <zhaozhiyu51@gmail.com>
2021-12-10 11:18:41 +0800

    before switching to windows

---
+++ 作业7/jsff7.f90
@@ -0,0 +1,4 @@
+program jsff7
+    implicit none
+    
+end program jsff7
---
+++ 作业7/xls2arr.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import re
+
+x = np.linspace(-2, 2, 17)
+y = np.linspace(-2, 2, 17)
+xygrid = np.meshgrid(x, y)
+
+U = pd.read_excel('u.xls', usecols=range(1, 18))
+V = pd.read_excel('v.xls', usecols=range(1, 18))
+print(U.values[1][1])
+print(U.shape)
+
+np.set_printoptions(linewidth=np.inf)
+with open('u.txt', 'w') as f:
+    # for i in range(17):
+        # f.write(str(U.values[i]) + '\n')
+    f.write(' ')
+    f.write(re.sub('[\[\]]', '', np.array_str(U.values)))
+
+f.close()
+
+with open('v.txt', 'w') as f:
+    # for i in range(17):
+        # f.write(str(U.values[i]) + '\n')
+    f.write(' ')
+    f.write(re.sub('[\[\]]', '', np.array_str(V.values)))
+
+f.close()
+
+
