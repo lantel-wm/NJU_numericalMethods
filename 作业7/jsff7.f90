836863ca91996a4cdb16605699aa0bcd835fe819
Zhao Zhiyu <zhaozhiyu51@gmail.com>
2021-12-10 23:42:23 +0800

    make run

--- 作业7/jsff7.f90
+++ 作业7/jsff7.f90
@@ -11,5 +11,6 @@ program jsff7
     call read_grid(grid_X, grid_Y)
     call calc_div(D, U, V, grid_X, grid_Y)
     call solve_equation(D, phi, 0.25_dp, 1e-7_dp)
+    print *, phi
     call calc_uv(phi,U_p, V_p, 0.25_dp)
 end program jsff7
