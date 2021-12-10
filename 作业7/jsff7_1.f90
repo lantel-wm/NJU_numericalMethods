a53806e955fb39626ca4918733df0f2b1094fbc2
Zhao Zhiyu <zhaozhiyu51@gmail.com>
2021-12-10 19:45:13 +0800

    make

--- 作业7/jsff7_1.f90
+++ 作业7/jsff7_1.f90
@@ -3,6 +3,4 @@ subroutine step1()
     real(8) :: U(17, 17), V(17, 17)
 
     call read_uv(U, V)
-    print *, U(1, 3)
-    print *, V(3, 1)
 end subroutine
--- 作业7/xls2arr.py
+++ 作业7/xls2arr.py
@@ -4,28 +4,23 @@ import re
 
 x = np.linspace(-2, 2, 17)
 y = np.linspace(-2, 2, 17)
-xygrid = np.meshgrid(x, y)
+grid = np.meshgrid(x, y)
 
 U = pd.read_excel('u.xls', usecols=range(1, 18))
 V = pd.read_excel('v.xls', usecols=range(1, 18))
-#  print(U.values[1][1])
-#  print(U.shape)
 
 np.set_printoptions(linewidth=np.inf)
 with open('u.txt', 'w') as f:
-    # for i in range(17):
-        # f.write(str(U.values[i]) + '\n')
     f.write(' ')
     f.write(re.sub('[\[\]]', '', np.array_str(U.values.transpose())))
-
-f.close()
+    f.close()
 
 with open('v.txt', 'w') as f:
-    # for i in range(17):
-        # f.write(str(U.values[i]) + '\n')
     f.write(' ')
     f.write(re.sub('[\[\]]', '', np.array_str(V.values.transpose())))
+    f.close()
 
-f.close()
-
-
+with open('gird.txt', 'w') as f:
+    f.write(' ')
+    f.write(re.sub('[\[\]]', '', np.array_str(grid.values.transpose())))
+    f.close()
