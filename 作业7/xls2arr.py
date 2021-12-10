792fa46aefcbcd0f6037539d1eac78fdea9f0ec2
Zhao Zhiyu <zhaozhiyu51@gmail.com>
2021-12-10 19:40:15 +0800

    make

--- 作业7/xls2arr.py
+++ 作业7/xls2arr.py
@@ -8,15 +8,15 @@ xygrid = np.meshgrid(x, y)
 
 U = pd.read_excel('u.xls', usecols=range(1, 18))
 V = pd.read_excel('v.xls', usecols=range(1, 18))
-print(U.values[1][1])
-print(U.shape)
+#  print(U.values[1][1])
+#  print(U.shape)
 
 np.set_printoptions(linewidth=np.inf)
 with open('u.txt', 'w') as f:
     # for i in range(17):
         # f.write(str(U.values[i]) + '\n')
     f.write(' ')
-    f.write(re.sub('[\[\]]', '', np.array_str(U.values)))
+    f.write(re.sub('[\[\]]', '', np.array_str(U.values.transpose())))
 
 f.close()
 
@@ -24,7 +24,7 @@ with open('v.txt', 'w') as f:
     # for i in range(17):
         # f.write(str(U.values[i]) + '\n')
     f.write(' ')
-    f.write(re.sub('[\[\]]', '', np.array_str(V.values)))
+    f.write(re.sub('[\[\]]', '', np.array_str(V.values.transpose())))
 
 f.close()
 
