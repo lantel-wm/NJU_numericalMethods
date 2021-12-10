12f6ea1bece795844bfac07658a5324b81859734
Zhao Zhiyu <zhaozhiyu51@gmail.com>
2021-12-10 21:25:25 +0800

    make run

--- 作业7/jsff7.f90
+++ 作业7/jsff7.f90
@@ -1,5 +1,6 @@
 program jsff7
     implicit none
+    integer, parameter :: dp = selected_real_kind(15)
     real(8) :: U(0:18, 0:18), V(0:18, 0:18)
     real(8) :: D(0:18, 0:18)
     real(8) :: phi(0:18, 0:18)
@@ -8,5 +9,5 @@ program jsff7
     call read_uv(U, V)
     call read_grid(grid_X, grid_Y)
     call calc_div(D, U, V, grid_X, grid_Y)
-    
+    call solve_equation(D, phi, 0.25_dp, 1e-7_dp)
 end program jsff7
--- 作业7/subroutines.f90
+++ 作业7/subroutines.f90
@@ -38,5 +38,29 @@ subroutine calc_div(D, U, V, grid_X, grid_Y)
 
 end subroutine calc_div 
 
-subroutine solve_equation()
+subroutine solve_equation(D, phi, h, eps)
+    implicit none
+    integer, parameter :: dp = selected_real_kind(15)
+    real(8), intent(in) :: D(0:18, 0:18), h, eps
+    real(8),intent(in out) ::  phi(0:18, 0:18)
+    real(8) :: R(0:18, 0:18)
+    real(8) :: alpha = 1.6_dp, diff = 1.0_dp
+    integer i, j
+    
+    do i = 0, 18
+        do j = 0, 18
+            phi(i, j) = 0.0_dp
+        end do
+    end do
+    do while(diff > eps)
+        diff = 1e-7_dp
+        do i = 1, 17
+            do j = 1, 17
+                R(i, j) = (phi(i + 1, j) + phi(i, j + 1) + phi(i - 1, j) + phi(i, j - 1) - 4 * phi(i, j)) / (h * h) - D(i, j)
+                phi(i, j) = phi(i, j) + 0.25_dp * alpha * R(i, j)
+                diff = max(diff, abs(0.25_dp * alpha * R(i, j)))
+            end do
+        end do
+    end do
+    
 end subroutine solve_equation
