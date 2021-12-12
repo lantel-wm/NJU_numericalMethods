cb3193bce3aac64117c1619a0b3da88cba77dbbf
Zhao Zhiyu <zhaozhiyu51@gmail.com>
2021-12-12 14:13:44 +0800

    make run

--- 作业7/plot_wind.py
+++ 作业7/plot_wind.py
@@ -11,6 +11,25 @@ dfV = pd.read_excel('v.xls', usecols=range(1, 18))
 U = dfU.values
 V = dfV.values
 
+up = []
+vp = []
+
+with open('up.txt', 'r') as f:
+    for line in f:
+        print(line.split())
+        up = list(map(float, line.split()))
+f.close()
+
+with open('vp.txt', 'r') as f:
+    for line in f:
+        print(line.split())
+        vp = list(map(float, line.split()))
+f.close()
+
+
+up = np.array(up).reshape(17, 17)
+vp = np.array(vp).reshape(17, 17)
+
 plt.subplots(figsize=(12, 8))
 
 plt.xlabel('X')
@@ -19,3 +38,26 @@ plt.ylabel('Y')
 plt.quiver(x, y, U, V)
 
 plt.savefig('wind.png')
+plt.close()
+
+plt.subplots(figsize=(12, 8))
+
+plt.xlabel('X')
+plt.ylabel('Y')
+
+plt.quiver(x, y, up, vp)
+
+plt.savefig('div_wind.png')
+plt.close()
+
+plt.subplots(figsize=(12, 8))
+
+plt.xlabel('X')
+plt.ylabel('Y')
+
+plt.quiver(x, y, U - up, V - vp)
+
+plt.savefig('vor_wind.png')
+plt.close()
+
+
