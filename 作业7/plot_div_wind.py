c9f7030dbfd0b73f50a13a0ccecf3b4b01f02b10
Zhao Zhiyu <zhaozhiyu51@gmail.com>
2021-12-12 13:44:08 +0800

    make run

--- 作业7/plot_div_wind.py
+++ 作业7/plot_div_wind.py
@@ -11,8 +11,24 @@ vp = []
 
 with open('up.txt', 'r') as f:
     for line in f:
-        print(type(line))
-        up.append(map(int, line.split(' ')))
-    f.close()
+        print(line.split())
+        up = list(map(float, line.split()))
+f.close()
 
-print(up)
+with open('vp.txt', 'r') as f:
+    for line in f:
+        print(line.split())
+        vp = list(map(float, line.split()))
+f.close()
+
+up = np.array(up).reshape(17, 17)
+vp = np.array(vp).reshape(17, 17)
+
+plt.subplots(figsize=(12, 8))
+
+plt.xlabel('X')
+plt.ylabel('Y')
+
+plt.quiver(x, y, up, vp)
+
+plt.savefig('div_wind.png')
