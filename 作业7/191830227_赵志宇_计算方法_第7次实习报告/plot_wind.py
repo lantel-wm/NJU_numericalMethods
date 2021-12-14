e422618eafd9423f4e2ad19d87a4aaf4d00908ba
Zhao Zhiyu <zhaozhiyu51@gmail.com>
2021-12-14 10:45:57 +0800

    make run

--- 作业7/191830227_赵志宇_计算方法_第7次实习报告/plot_wind.py
+++ 作业7/191830227_赵志宇_计算方法_第7次实习报告/plot_wind.py
@@ -53,7 +53,7 @@ plt.subplots(figsize=(12, 8))
 plt.xlabel('X')
 plt.ylabel('Y')
 
-contour = plt.contour(x, y, D, np.arange(-4, 3.5, 0.5), colors='gray', linestyles='-', labels='Divergence')
+contour = plt.contour(x, y, D, np.arange(-4, 3.5, 0.5), colors='gray', linestyles='-', label='Divergence')
 plt.clabel(contour, fontsize=10, colors='gray')
 plt.quiver(x, y, U, V)
 plt.title('Original Wind Field')
